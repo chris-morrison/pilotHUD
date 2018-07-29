8edf47e8ed0fd5054ecffa00ce6ee8d4c68ae757
Chris Morrison <cjmorrison@gmail.com>
2018-07-29 15:12:25 +1000

    Broke the pilotHUD user control out into its own class, with a separate sample project that uses it

--- pilotHUD_Sample/MainWindow.xaml.cs
+++ pilotHUD_Sample/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace pilotHUD
+namespace pilotHUD_Sample
 {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
